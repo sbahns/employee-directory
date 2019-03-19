e177561cda58e3b75ae7675c145964302b7f8b06
Sarah Barnes <sarah@sarahbarnesweb.com>
2019-03-18 22:27:24 -0400

    add modal
    TODO: add navigation between profiles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const grid = document.querySelector('.grid');
-const url = 'https://randomuser.me/api/?results=12&nat=us'; // Get 12 random users
+const card = document.querySelector('.card');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modal_content = document.querySelector('.modal_content');
+
+const url = 'https://randomuser.me/api/?results=12&nat=us&seed=srbweb'; // Get 12 random users
 
 function createNode(element) {
   return document.createElement(element); // Create the type of element you pass in the parameters
@@ -9,12 +14,35 @@ function append(parent, el) {
   return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
+function attachModalListeners(modalElm) {
+  modalElm.querySelector('.close_modal').addEventListener('click', toggleModal);
+  modalElm.querySelector('.overlay').addEventListener('click', toggleModal);
+}
+
+function detachModalListeners(modalElm) {
+  modalElm.querySelector('.close_modal').removeEventListener('click', toggleModal);
+  modalElm.querySelector('.overlay').removeEventListener('click', toggleModal);
+}
+
+function toggleModal() {
+  var currentState = modal.style.display;
+
+  // If modal is visible, hide it. Else, display it.
+  if (currentState === 'none') {
+    modal.style.display = 'block';
+    attachModalListeners(modal);
+  } else {
+    modal.style.display = 'none';
+    detachModalListeners(modal);
+  }
+}
+
 function loopEmployees(x) {
 	let cell = createNode('div');
 	cell.className ="cell";
 	const html = `
 			<div class="card">
-				<div class="user">
+				<div class="user" id="${x.id.value}">
 					<div class="avatar">
 						<img src='${x.picture.medium}' />
 					</div>
@@ -45,7 +73,14 @@ function loopEmployees(x) {
 
 	 cell.innerHTML = html;
 	 append(grid, cell);
+
+	 cell.addEventListener('click', (e) => {
+		toggleModal();
+		append(modal_content, cell);
+	 });
 }
+
+
 
 function getEmployees(data) {
 	let employees = data.results;
@@ -54,6 +89,11 @@ function getEmployees(data) {
 	});
 }
 
+// function profileOverlay() {
+// 	card.addEventListener('click', (e) => {
+// 		overlay.classList.add('is-active');
+// 	});
+// }
 
 fetch(url)
 	.then(response => response.json())
