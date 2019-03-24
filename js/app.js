2afd1237ed51cf93449f1a81b7dfd9127a3455b8
Sarah Barnes <sarah@sarahbarnesweb.com>
2019-03-24 15:54:56 -0400

    working modal
    TODO: add working previous/next links

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const modal_content = document.querySelector('.modal_content');
 
-const url = 'https://randomuser.me/api/?results=12&nat=us&seed=srbweb'; // Get 12 random users
+const json = 'https://randomuser.me/api/?results=12&nat=us&seed=srbweb'; // Get 12 random users
+
+let employee = [];
+let selectedProfile = 0;
 
 function createNode(element) {
   return document.createElement(element); // Create the type of element you pass in the parameters
@@ -14,6 +17,118 @@ function append(parent, el) {
   return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+function fetchData(url) {
+  return fetch(url)
+           .then(res => res.json())
+}
+
+fetchData(json)
+  .then(getEmployees);
+
+fetchData(json)
+	.then(function(data) {
+				employee = data.results;
+	      let cell = document.querySelectorAll('.cell');
+	        for (let i=0; i<cell.length; i++) {
+	        cell[i].addEventListener('click', () => {
+	          if (modal.className=='modal') {
+	              selectedProfile = i;
+	              document.querySelector('.modal_content').innerHTML = employeeModal(employee, i);
+
+	          	}
+							toggleModal();
+	          });
+
+	        }
+			});
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+
+function employeeModal(array, index) {
+	return `
+				<button title="Close" class="close_modal">
+					X
+				</button>
+
+				<div>
+				    <span class="prev"> < </span>
+				    <span class="next"> > </span>
+				</div>
+				<div class="card" data-id="${array[index]}">
+					<div class="user">
+						<div class="avatar">
+							<img src='${array[index].picture.medium}' />
+						</div>
+						<div class='user-info'>
+							<div class="name">${array[index].name.first} ${array[index].name.last}</div>
+							<div class="email">
+								${array[index].email}
+							</div>
+							<div class="city">
+								${array[index].location.city}
+							</div>
+							<div class="reveal-on-modal">
+								<div class="phone">
+									${array[index].cell}
+								</div>
+								<div class="address">
+									${array[index].location.street}
+									${array[index].location.city}, ${array[index].location.state} ${array[index].location.postcode}
+								</div>
+								<div class="birthdate">
+									${array[index].dob.date}
+								</div>
+							</div>
+						</div>
+					</div>
+
+			</div>`;
+
+}
+
+function getEmployees(data) {
+	let employees = data.results;
+	employees.forEach((employee, index) => {
+		let cell = createNode('div');
+		cell.className ="cell";
+		const html = `
+				<div class="card" data-id="${index}">
+					<div class="user">
+						<div class="avatar">
+							<img src='${employee.picture.medium}' />
+						</div>
+						<div class='user-info'>
+							<div class="name">${employee.name.first} ${employee.name.last}</div>
+							<div class="email">
+								${employee.email}
+							</div>
+							<div class="city">
+								${employee.location.city}
+							</div>
+						</div>
+					</div>
+
+			</div>`;
+
+		 cell.innerHTML = html;
+		 append(grid, cell);
+	});
+}
+
+
+
+
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
 function attachModalListeners(modalElm) {
   modalElm.querySelector('.close_modal').addEventListener('click', toggleModal);
   modalElm.querySelector('.overlay').addEventListener('click', toggleModal);
@@ -26,7 +141,6 @@ function detachModalListeners(modalElm) {
 
 function toggleModal() {
   var currentState = modal.style.display;
-
   // If modal is visible, hide it. Else, display it.
   if (currentState === 'none') {
     modal.style.display = 'block';
@@ -36,68 +150,3 @@ function toggleModal() {
     detachModalListeners(modal);
   }
 }
-
-function loopEmployees(x) {
-	let cell = createNode('div');
-	cell.className ="cell";
-	const html = `
-			<div class="card">
-				<div class="user" id="${x.id.value}">
-					<div class="avatar">
-						<img src='${x.picture.medium}' />
-					</div>
-					<div class='user-info'>
-						<div class="name">${x.name.first} ${x.name.last}</div>
-						<div class="email">
-							${x.email}
-						</div>
-						<div class="city">
-							${x.location.city}
-						</div>
-						<div class="reveal-on-modal">
-							<div class="phone">
-								${x.cell}
-							</div>
-							<div class="address">
-								${x.location.street}
-								${x.location.city}, ${x.location.state} ${x.location.postcode}
-							</div>
-							<div class="birthdate">
-								${x.dob.date}
-							</div>
-						</div>
-					</div>
-				</div>
-
-		</div>`;
-
-	 cell.innerHTML = html;
-	 append(grid, cell);
-
-	 cell.addEventListener('click', (e) => {
-		toggleModal();
-		append(modal_content, cell);
-	 });
-}
-
-
-
-function getEmployees(data) {
-	let employees = data.results;
-    return employees.map(function(employee) {
-		loopEmployees(employee);
-	});
-}
-
-// function profileOverlay() {
-// 	card.addEventListener('click', (e) => {
-// 		overlay.classList.add('is-active');
-// 	});
-// }
-
-fetch(url)
-	.then(response => response.json())
-	.then(getEmployees)
-	.catch(function(error) {
-	// If there is any error you will catch them here
-	});
